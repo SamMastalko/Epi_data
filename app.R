dabe699895ba06c8d100a85752ea9815a05f75bd
SamMastalko <sammastalko@gmail.com>
2021-07-17 20:12:33 +0200

    app update

--- app.R
+++ app.R
@@ -14,22 +14,43 @@ ui <- dashboardPage(skin = "yellow",
     dashboardHeader(title = "Epi data"),
     dashboardSidebar(
         sidebarMenu(
-            menuItem("Nákaza kumulativní", tabName = "nakaza_cumul", icon = icon("medkit"))
+            menuItem("Nákaza", tabname = "nakaza", icon = icon("medkit"),
+                menuSubItem("Nákaza kumulativní", tabName = "nakaza_cumul"),
+                menuSubItem("Aktuálně nakažených", tabName = "nakaza_actual")
+            )
         )
     ),
     dashboardBody(
         tabItems(
             tabItem(tabName = "nakaza_cumul",
                     fluidRow(
-                        box(dateRangeInput("date_nakaza_cumul",
+                        headerPanel("Kumulativní nákaza"),
+                        box(title = "Možnosti", status = "warning", solidHeader = TRUE,
+                            dateRangeInput("date_nakaza_cumul",
                                        "Zvolte datum",
                                        start = "2020-01-27",
                                        end = Sys.Date(),
                                        min = "2020-01-27",
                                        max = Sys.Date()),
                             textOutput("popis_cumul"),
-                            width = 3),
-                        box(plotly::plotlyOutput("nakaza_kumulativni_plot"))
+                            width = 4),
+                        box(title = "Graf", status = "primary", solidHeader = TRUE,
+                            plotly::plotlyOutput("nakaza_kumulativni_plot"), width = 8)
+                    )),
+            tabItem(tabName = "nakaza_actual",
+                    fluidRow(
+                        headerPanel("Aktuální nákaza"),
+                        box(title = "Možnosti", status = "warning", solidHeader = TRUE,
+                            dateRangeInput("date_nakaza_actual",
+                                           "Zvolte datum",
+                                           start = "2020-01-27",
+                                           end = Sys.Date(),
+                                           min = "2020-01-27",
+                                           max = Sys.Date()),
+                            textOutput("popis_actual"),
+                            width = 4),
+                        box(title = "Graf", status = "primary", solidHeader = TRUE,
+                            plotly::plotlyOutput("nakaza_aktualni_plot"), width = 8)
                     ))
         )
     )
@@ -53,6 +74,25 @@ server <- function(input, output, session) {
     })
     output$popis_cumul <- renderText({
         "Graf zobrazuje kumulativní nákazu podle hlášení hygienických stanic."
+    })
+    
+    nakaza_aktualni_plot <- reactive({
+        nakazeni_vyleceni_umrti_testy %>%
+            mutate(nakazeni_aktualne = kumulativni_pocet_nakazenych - kumulativni_pocet_umrti - kumulativni_pocet_vylecenych)%>%
+            filter(datum >= input$date_nakaza_actual[1], datum <= input$date_nakaza_actual[2])%>%
+            ggplot(aes(datum, nakazeni_aktualne))+
+            geom_line(color = "gray16")+
+            scale_x_date(date_breaks = "1 month", date_labels = "%B %Y")+
+            theme(axis.text.x = element_text(angle = 90, hjust = 1))+
+            xlab("Datum")+
+            ylab("Aktuální počet nakažených")
+    })
+    
+    output$nakaza_aktualni_plot <- plotly::renderPlotly({
+        nakaza_aktualni_plot()
+    })
+    output$popis_actual <- renderText({
+        "Graf zobrazuje aktuální nákazu podle hlášení hygienických stanic."
     })
    
 }
